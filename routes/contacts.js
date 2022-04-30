5f5c9ce1e463dc63e2ea6a097977a0ff8a740d46
ejgdr <gui20006@byui.edu>
2022-04-30 11:37:39 -0600

    Cleaned up db code

--- routes/contacts.js
+++ routes/contacts.js
@@ -1,22 +1,13 @@
 const routes = require('express').Router();
-
+const connect = require('../db/connect');
 routes.get('/', (req, res) => {
-    
-    // Ugly code
-
-    const dotenv = require('dotenv');
-    dotenv.config();
-
-    const MongoClient = require('mongodb').MongoClient;
-    MongoClient.connect(process.env.MONGODB_URI, function(err, db){
-        if (err) throw err;
-        var dbo = db.db("contacts");
-        dbo.collection("contacts").find().toArray(function(err, result){
-            if(err) throw err;
-            res.json(result);
-            db.close();
-        });
+      
+    connect.getCollection().find().toArray((err, result) => {
+        if(err) throw err;
+        res.json(result);
+        console.log("Contacts Query Successful")
     });
 });
+
 
 module.exports = routes;
