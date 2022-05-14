c703c73b66f1122a8a850f908469eefc3c8a722b
ejgdr <gui20006@byui.edu>
2022-05-14 11:39:55 -0600

    Adding documentation for week4, all working

--- routes/index.js
+++ routes/index.js
@@ -1,5 +1,7 @@
 const routes = require('express').Router();
 
+routes.use('/', require('./swagger'));
 routes.use('/', require('./home'));
 routes.use('/contacts', require('./contacts'));
+
 module.exports = routes;
